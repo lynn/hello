cca54b6d9f4f5309572336576366bdf63ef1b86e
Lynn <lynn@foldr.moe>
2022-02-20 21:08:02 +0100

    Fix local-time seed generation

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,11 @@ function useSetting<T>(
   return [current, setSetting];
 }
 
-const todaySeed = new Date().toLocaleDateString().replace(/\//g, "");
+const now = new Date();
+const todaySeed =
+  now.toLocaleDateString("en-US", { year: "numeric" }) +
+  now.toLocaleDateString("en-US", { month: "2-digit" }) +
+  now.toLocaleDateString("en-US", { day: "2-digit" });
 
 function App() {
   type Page = "game" | "about" | "settings";
